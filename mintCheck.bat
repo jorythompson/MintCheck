6f0c2da96e273dfcb197ccd64f4ca030982de636
Jordan Thompson <Jordan@ThompCo.com>
2017-01-22 14:08:52 -0500

    minor updates

--- mintCheck.bat
+++ mintCheck.bat
@@ -1,2 +1,2 @@
-python \\andraia\jordan\pythonDevelopment\mintToEvernote\MintCheck\mintCheck.py --config \\andraia\jordan\pythonDevelopment\mintToEvernote\MintCheck\home.ini --live
+python \\andraia\jordan\pythonDevelopment\\MintCheck\mintCheck.py --config \\andraia\jordan\pythonDevelopment\MintCheck\home.ini --live
 exit
---
+++ mintCheck.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python /home/jordan/pythonDevelopment/MintCheck/mintCheck.py --config /home/jordan/pythonDevelopment/MintCheck/home.ini 
+#python /home/jordan/pythonDevelopment/MintCheck/mintCheck.py --config /home/jordan/pythonDevelopment/MintCheck/home.ini --live
