fa0590910365d36c82036c6c66b2368bc48d4808
Jordan Thompson <Jordan@ThompCo.com>
2018-07-04 19:45:07 -0400

    got it working on debian

--- mintCheck.sh
+++ mintCheck.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+export PATH=$PATH:/usr/lib/chromium-browser
 export PYTHONPATH=/home/jordan/pythonDevelopment/github
-#python /home/jordan/pythonDevelopment/MintCheck/mintCheck.py --config /home/jordan/pythonDevelopment/MintCheck/home.ini --live
-python /home/jordan/pythonDevelopment/MintCheck/mintCheck.py --config /home/jordan/pythonDevelopment/MintCheck/home.ini 
+export PYTHONPATH=/mnt/usbdrive/python/github
+#python /mnt/usbdrive/python/MintCheck/mintCheck.py --config /mnt/usbdrive/python/MintCheck/home.ini --live
+#python /mnt/usbdrive/python/MintCheck/mintCheck.py --config /mnt/usbdrive/python/MintCheck/home.ini --prompt_for_text
+python /mnt/usbdrive/python/MintCheck/mintCheck.py --config /mnt/usbdrive/python/MintCheck/home.ini
 exit
