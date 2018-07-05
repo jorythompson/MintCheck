33dfaf9adc210c2b513cde6e8b12462a60e22687
= <=>
2018-07-05 18:23:53 -0400

    cleaning up some code that is no longer used

--- mintCheck.sh
+++ mintCheck.sh
@@ -2,7 +2,8 @@
 export PATH=$PATH:/usr/lib/chromium-browser
 export PYTHONPATH=/home/jordan/pythonDevelopment/github
 export PYTHONPATH=/mnt/usbdrive/python/github
-#python /mnt/usbdrive/python/MintCheck/mintCheck.py --config /mnt/usbdrive/python/MintCheck/home.ini --live
-#python /mnt/usbdrive/python/MintCheck/mintCheck.py --config /mnt/usbdrive/python/MintCheck/home.ini --prompt_for_text
+# use this line to prompt mint to send a text message for a passcode.  Note that mint will only send a passcode (this
+# will only work) if the previous one has expired.
+# python /mnt/usbdrive/python/MintCheck/mintCheck.py --config /mnt/usbdrive/python/MintCheck/home.ini --prompt_for_text
 python /mnt/usbdrive/python/MintCheck/mintCheck.py --config /mnt/usbdrive/python/MintCheck/home.ini
 exit
